03396a9240e6c3b5f55dd256e25cad46a148e222
João Afonso <ferreiraafonsojoao@ua.pt>
2023-09-19 15:21:22 +0100

    WAVHist Class

--- sndfile-example-src/wav_hist.h
+++ sndfile-example-src/wav_hist.h
@@ -9,10 +9,14 @@
 class WAVHist {
   private:
 	std::vector<std::map<short, size_t>> counts;
+    std::vector<std::map<short, size_t>> midChannelCounts;
+    std::vector<std::map<short, size_t>> sideChannelCounts;
 
   public:
-	WAVHist(const SndfileHandle& sfh) {
+    WAVHist(const SndfileHandle& sfh) {
 		counts.resize(sfh.channels());
+		midChannelCounts.resize(sfh.channels());
+		sideChannelCounts.resize(sfh.channels());
 	}
 
 	void update(const std::vector<short>& samples) {
@@ -25,6 +29,19 @@ class WAVHist {
 		for(auto [value, counter] : counts[channel])
 			std::cout << value << '\t' << counter << '\n';
 	}
+
+	void updateSamples(const std::vector<short>& samples) {
+		size_t n { };
+		for(auto s : samples)
+			counts[n++ % counts.size()][s]++; 
+		if(counts.size() == 2) {
+			short mid = (samples[0] + samples[1]) / 2;  // (L + R) / 2, also known as the MID channel)
+			short side = (samples[0] - samples[1]) / 2; // (L − R) / 2, also known as the SIDE channel
+			midChannelCounts[0][mid]++; 
+			sideChannelCounts[0][side]++; 
+		}
+	}
+
 };
 
 #endif
