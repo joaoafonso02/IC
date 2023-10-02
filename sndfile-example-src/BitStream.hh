9e39e9be81d1c4e3c7bbb435dd239d39cdbdd04f
EduardoFernandesUA <edu.fernandes@ua.pt>
2023-10-02 15:49:08 +0100

    write bit

--- sndfile-example-src/BitStream.hh
+++ sndfile-example-src/BitStream.hh
@@ -5,72 +5,51 @@ class BitStream {
 private:
   std::fstream fs;
   std::streambuf* filebuf;
-  long fileSize;
+
+  unsigned char buffer;
+  short bufferpos;
+
 public:
+  enum Flag {w, r};
+
   // Constructor
-  BitStream(std::string fileName) {
-    fs.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
+  BitStream(std::string fileName, Flag flag) {
+    if( flag==r ) fs.open(fileName, std::ios::in | std::ios::binary);
+    if( flag==w ) fs.open(fileName, std::ios::out | std::ios::binary);
     filebuf = fs.rdbuf();
 
-    // get file size
-    fs.seekg(0, fs.end);
-    fileSize = fs.tellg();
-    printf("FILE SIZE %ld\n", fileSize);
-    fs.seekg(fs.beg);
+    buffer = 0;
+    bufferpos = 0;
   }
-  // BitStream(std::string fileName) {
-  //   os.open(fileName, std::ios::out | std::ios::binary);
 
-  //   if (!os) {
-  //       std::cerr << "Failed to open the file '" << fileName << "' for output" << std::endl;
-  //       throw std::runtime_error("Failed to open the file for output");
-  //   }
+  void writeBit(bool bit) {
+    unsigned char cbit = (unsigned char) bit;
+    cbit <<= 7-bufferpos;
+    buffer |= cbit;
+    bufferpos += 1;
 
-  //   // Initialize the filebuf
-  //   filebuf = os.rdbuf();
-
-  //   // Reset fileSize to 0 since you are writing to a new file
-  //   fileSize = 0;
-  // }
-
-
-  char* readBits(unsigned int n, unsigned int offset) {
-    if( n>64 ) throw std::invalid_argument("[BitStream->readBits] n must be less than 64");
-    if( offset+n>fileSize*8 ) throw std::invalid_argument("[BitStream->readBits] offset greater than file, this file max size is " + std::to_string(fileSize));
-    if( offset%8!=0 ) throw std::invalid_argument("[BitStream->readBits] offset not divisible by 8 not implemented");
-
-    // TODO: currently only accepts offset that are divisible by 8, to change that the read needs to start in the middle of a byte
-
-    fs.seekg(offset / 8, fs.beg);
-
-    // read bytes
-    unsigned int byteCount = n/8 + (n%8!=0); 
-    char* bits = (char*) malloc(byteCount);
-    filebuf->sgetn(bits, byteCount);
-
-    // remove excess bits from last byte
-    if(n%8!=0) {
-      unsigned char mask = -1 << (8-n%8);
-      bits[byteCount-1] = bits[byteCount-1] & mask;
+    printf("buffer: %x\n", buffer);
+    if(bufferpos==8) {
+      filebuf->sputc(buffer);
+      buffer = 0;
+      bufferpos = 0;
     }
-
-    return bits;
   }
 
-  void writeBits(const char* data, unsigned int n, unsigned int offset) {
-    fs.seekp(offset / 8, fs.beg); // Move the output stream to the correct byte position
-
-    // Write bits
-    unsigned int byteCount = n / 8 + (n % 8 != 0); // Calculate the number of bytes to write
-    filebuf->sputn(data, byteCount);
-     // Write the data to the file
-
-    // Flush the stream to ensure data is written immediately
-    fs.flush();
+  void writeBitFlush() {
+    filebuf->sputc(buffer);
+    buffer = 0;
+    bufferpos = 0;
   }
 
-
-  long getFileSize() { return fileSize; }
+  void writeBits(unsigned char* data, unsigned int n, unsigned int offset) {
+    for(int i=0; i<n; i++) {
+      bool bit = (data[i/8] >> (7-i%8)) & 0x01;
+      printf("%b [%d, %d]: %d\n", data[i/8], i/8, i%8, bit);
+      writeBit(bit);
+    }
+    writeBitFlush();
+  }
 
   // Deconstructor
   ~BitStream() {
--- sndfile-example-src/test_BitStream.cpp
+++ sndfile-example-src/test_BitStream.cpp
@@ -10,38 +10,15 @@ int main(int argc, char **argv) {
   }
 
   std::string fileName = argv[1];
-  unsigned int bitCount =11;
-
-  // Check if the specified file exists, and create it if it doesn't
-  std::ifstream fileExists(fileName);
-  if (!fileExists) {
-    // The file doesn't exist; create it
-    std::ofstream createFile(fileName, std::ios::binary | std::ios::trunc);
-    if (!createFile) {
-      std::cerr << "Failed to create the file '" << fileName << "'" << std::endl;
-      return 1; // Return an error code
-    }
-    createFile.close();
-  }
+  unsigned int bitCount = 11;
 
   // Create a BitStream instance 
-  BitStream file(fileName);
+  BitStream file(fileName, BitStream::w);
 
   // write A7 and C0 in binary
-  unsigned char data[] = {0xA7, 0xC0};
+  unsigned char data[] = {0xA7, 0xC3};
 
-  file.writeBits(reinterpret_cast<const char*>(data), bitCount, 0);
-
-  // return first 11 bits from this file
-  char* bits = file.readBits(bitCount, 0);
-  std::cout << "Binary representation: ";
-  for (unsigned int i = 0; i < bitCount; i++) {
-    std::cout << ((bits[i / 8] >> (7 - (i % 8))) & 1);
-  }
-  std::cout << std::endl;
-
-
-  
+  file.writeBits(data, bitCount, 0);
 
   return 0;
 }
