2e3c4cabbc349772475e1caf53d11109ec7d191b
EduardoFernandesUA <edu.fernandes@ua.pt>
2023-11-07 00:29:02 +0000

    fixed gitignore

---
+++ project02/src/extract_channel.cpp
@@ -0,0 +1,40 @@
+#include <opencv2/opencv.hpp> 
+#include <stdio.h> 
+
+int main(int argc, char** argv) 
+{ 
+    if (argc != 4) { 
+        printf("usage: extract_channel <in_image_path> <channel> <out_image_path>\n"); 
+        return -1; 
+    } 
+
+    cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR); 
+    if (!image.data) { 
+        printf("No image data \n"); 
+        return -1; 
+    } 
+
+    cv::Mat out {image.size(), CV_8UC1};
+
+    int y, x, c;
+    for(y=0; y<image.rows; y++) {
+        for(x=0; x<image.cols; x++){
+            c = std::stoi(argv[2]);
+            out.at<unsigned char>(y,x) = image.at<cv::Vec3b>(y, x)[c];
+        }
+    }
+
+    cv::imwrite(argv[3], out);
+
+    return 0; 
+}
+/*
+    cv::Mat out {image.size(), CV_8UC1};
+
+    int y, x;
+    for(y=0; y<image.rows; y++) {
+        for(x=0; x<image.cols; x++){
+            image.at<unsigned char>(y,x) = image.at<cv::Vec3b>(y, x)[std::stoi(argv[2])];
+        }
+    }
+*/
